bf433d5e98406f77893f282bb22dfc9cc1a10339
devSaifur <saifur2akash@gmail.com>
2024-05-05 20:03:41 +0600

    setup font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { cn } from './lib/utils'
+
 function App() {
-  return <div className="bg-black">hello vite</div>
+  return (
+    <div className={cn('font-geist h-screen w-full bg-black text-white')}>
+      hello vite
+    </div>
+  )
 }
 
 export default App
