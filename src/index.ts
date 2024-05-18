115146f0dd9fa6be7cb4c978e7cbf996e887e312
devSaifur <saifur2akash@gmail.com>
2024-05-18 16:19:04 +0600

    add auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,17 @@
-import { lucia } from './lib/auth'
+import { authMiddleware } from './middleware'
 import { authRoute } from './routes/auth'
 import { expensesRoute } from './routes/expenses'
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
-import { getCookie } from 'hono/cookie'
 import { csrf } from 'hono/csrf'
-import { logger } from 'hono/logger'
 import type { Session, User } from 'lucia'
 
-const app = new Hono<{
-    Variables: {
-        user: User | null
-        session: Session | null
-    }
-}>()
-
-app.use('*', logger())
-app.use(csrf())
+const app = new Hono<{ Variables: { user: User | null; session: Session | null } }>()
 
 const apiRoutes = app.basePath('/api').route('/expenses', expensesRoute).route('/auth', authRoute)
 
-app.use('*', async (c, next) => {
-    const sessionId = getCookie(c, lucia.sessionCookieName) ?? null
-    if (!sessionId) {
-        c.set('user', null)
-        c.set('session', null)
-        return next()
-    }
-    const { session, user } = await lucia.validateSession(sessionId)
-    if (session && session.fresh) {
-        c.header('Set-Cookie', lucia.createSessionCookie(session.id).serialize(), {
-            append: true,
-        })
-    }
-    if (!session) {
-        c.header('Set-Cookie', lucia.createBlankSessionCookie().serialize(), {
-            append: true,
-        })
-    }
-    c.set('user', user)
-    c.set('session', session)
-    return next()
-})
+app.use(csrf())
+app.use('*', authMiddleware)
 
 app.get('*', serveStatic({ path: './client/dist/index.html' }))
 app.use('*', serveStatic({ root: './client/dist' }))
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,21 +8,17 @@ const adapter = new DrizzleSQLiteAdapter(db, sessions, users)
 export const lucia = new Lucia(adapter, {
     sessionCookie: {
         attributes: {
-            // set to `true` when using HTTPS
             secure: process.env.NODE_ENV === 'production',
         },
     },
     getUserAttributes: (attributes) => {
         return {
-            // attributes has the type of DatabaseUserAttributes
             name: attributes.name,
             email: attributes.email,
             email_verified: attributes.emailVerified,
         }
     },
 })
-
-// IMPORTANT!
 declare module 'lucia' {
     interface Register {
         Lucia: typeof lucia
