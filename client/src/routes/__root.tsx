fc146aba59d65c85e69fce05e88468df1fda2478
devSaifur <saifur2akash@gmail.com>
2024-05-19 15:25:34 +0600

    rm devtools for prod

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import Navbar from '@/components/navbar'
 import { type QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 interface MyRouteContext {
   queryClient: QueryClient
@@ -18,8 +16,6 @@ function Root() {
       <Navbar />
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
-      <ReactQueryDevtools initialIsOpen={false} />
     </div>
   )
 }
