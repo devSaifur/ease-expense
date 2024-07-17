5d5c1c16cadd1ec9312746258e52db61fe6edb61
devSaifur <saifur2akash@gmail.com>
2024-07-18 03:08:19 +0600

    add charts

--- client/src/routes/_authenticated/index.lazy.tsx
+++ client/src/routes/_authenticated/index.lazy.tsx
@@ -1,9 +1,41 @@
+import type { ChartConfig } from '@/components/ui/chart'
+import { ChartContainer } from '@/components/ui/chart'
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Bar, BarChart } from 'recharts'
 
 export const Route = createLazyFileRoute('/_authenticated/')({
   component: () => <HomePage />,
 })
 
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: '#2563eb',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: '#60a5fa',
+  },
+} satisfies ChartConfig
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+]
+
 function HomePage() {
-  return <div className="mt-20 flex justify-center">Hello /!</div>
+  return (
+    <div className="mx-auto mt-20 flex max-w-lg flex-col">
+      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+        <BarChart accessibilityLayer data={chartData}>
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
+    </div>
+  )
 }
